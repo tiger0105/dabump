566997700999e9dd14bbfa4fa3d7f6f5b8f2b40f
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2020-08-07 18:19:23 -0400

    firebase firestore integration

--- Assets/Resources/Scripts/Courts.cs
+++ Assets/Resources/Scripts/Courts.cs
@@ -1,10 +1,12 @@
-﻿using Firebase.Firestore;
+﻿using Firebase.Extensions;
+using Firebase.Firestore;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class Courts : MonoBehaviour
 {
+    [SerializeField] private GameObject m_CourtsLoadingBar;
     public TextMeshProUGUI debugText;
 
     public static Courts Instance;
@@ -14,21 +16,31 @@ public class Courts : MonoBehaviour
         Instance = this;
     }
 
-    private void Start()
+    public async void GetCourts()
     {
-        
-    }
+        m_CourtsLoadingBar.SetActive(true);
 
-    public void GetCourts()
-    {
         FirebaseFirestore firestore = FirebaseFirestore.GetInstance(Firebase.FirebaseApp.DefaultInstance);
 
         CollectionReference courtsRef = firestore.Collection("Courts");
-        Query query = courtsRef.OrderByDescending("ID");
+        Query query = courtsRef.OrderBy("ID");
 
-        ListenerRegistration listener = query.Listen(snapshot =>
+        await query.GetSnapshotAsync().ContinueWithOnMainThread(task =>
         {
-            foreach (DocumentSnapshot documentSnapshot in snapshot.Documents)
+            if (task.IsCanceled)
+            {
+                DebugLog("GetCourts was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                DebugLog("GetCourts encountered an error: " + task.Exception);
+                return;
+            }
+
+            QuerySnapshot allCourtsSnapshot = task.Result;
+
+            foreach (DocumentSnapshot documentSnapshot in allCourtsSnapshot.Documents)
             {
                 Dictionary<string, object> court = documentSnapshot.ToDictionary();
                 DebugLog(court["ID"].ToString());
@@ -37,6 +49,8 @@ public class Courts : MonoBehaviour
                 DebugLog(court["Address"].ToString());
             }
         });
+
+        m_CourtsLoadingBar.SetActive(false);
     }
 
     private void DebugLog(string text)
