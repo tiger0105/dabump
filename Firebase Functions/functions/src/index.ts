46be0fb4fb0fb447ff476197e1882364efd539f7
Rey Santana Socorro <58710601+reysantana@users.noreply.github.com>
2020-08-11 05:42:32 -0400

    firebase functions added storage - issue

--- Firebase Functions/functions/src/index.ts
+++ Firebase Functions/functions/src/index.ts
@@ -2,15 +2,33 @@ import * as functions from 'firebase-functions';
 import admin = require('firebase-admin');
 
 admin.initializeApp();
+
 const firestore = admin.firestore();
+const storage = admin.storage();
 
 export const fetchCourts = functions.https.onRequest(async (request, response) => {
     const courtsReference = firestore.collection('Courts');
     const snapshot = await courtsReference.get();
-    let data: FirebaseFirestore.DocumentData[] = [];
-    snapshot.forEach(doc => {
-        console.log(doc.id, '=>', doc.data());
-        data.push(doc.data());
+    const data: FirebaseFirestore.DocumentData[] = [];
+    snapshot.forEach(async doc => {
+        const docData = doc.data();
+        const document = storage.bucket('dabump-8c59c.appspot.com').file(docData.Image);
+        const [url] = await document.getSignedUrl({
+            action: 'read',
+            expires: '08-13-2020',
+        });
+        docData.Image = url;
+        data.push({ 'court': docData });
     });
-    response.send("success");
+    response.send(data);
+});
+
+export const fetchProfiles = functions.https.onRequest(async (request, response) => {
+    const profilesReference = firestore.collection('Profiles');
+    const snapshot = await profilesReference.get();
+    const data: FirebaseFirestore.DocumentData[] = [];
+    snapshot.forEach(doc => {
+        data.push({ 'profile': doc.data() });
+    });
+    response.send(Object.assign({}, data));
 });
