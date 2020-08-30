933d02b498beae23af17144025ad0147135bf254
reysantanasocorro <rey@criterion.net>
2020-08-30 01:20:59 -0700

    image worked on ios - comfirmed

--- Assets/Resources/Scripts/Courts.cs
+++ Assets/Resources/Scripts/Courts.cs
@@ -489,7 +489,7 @@ public class Courts : MonoBehaviour
         if (trans == null)
             return;
 
-        FileInfo fileInfo = new FileInfo("file://" + imagePath);
+        FileInfo fileInfo = new FileInfo(imagePath);
 
         if (fileInfo == null || !fileInfo.Exists) return;
 
